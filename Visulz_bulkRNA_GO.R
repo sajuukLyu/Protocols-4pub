51f77dd9d55328c144d2bf3acd77282e90959de9
QTC.Yulin <lvyulin@pku.edu.cn>
2021-04-02 15:45:08 +0800

    <feat> add bulkRNA GO analysis and visualization

---
+++ Visulz_bulkRNA_GO.R
@@ -0,0 +1,55 @@
+
+# MESSAGE -----------------------------------------------------------------
+#
+# author: Yulin Lyu
+# email: lvyulin@pku.edu.cn
+#
+# require: R whatever
+#
+# ---
+
+# * 1. Load packages ------------------------------------------------------
+
+setwd("exampleData/RNA")
+
+# grammar
+library(tidyverse)
+library(magrittr)
+library(glue)
+library(data.table)
+
+# analysis
+library(DESeq2)
+library(org.Hs.eg.db)
+library(clusterProfiler)
+
+# graphics
+library(ggplot2)
+library(ggsci)
+library(latex2exp)
+library(patchwork)
+
+# * 2. Load data ----------------------------------------------------------
+
+GOfile <- list.files(".", "GO.csv")
+
+GOdata <- map(GOfile, ~ fread(.x)) %>% set_names(str_remove(GOfile, ".GO.*"))
+plotData <- GOdata %>% imap(~ {.x[pvalue < 0.05 & Count >= 5][1:20][, type := .y]})
+plotData %<>% map(~ {.x[, p := -log10(pvalue)]})
+
+GOplot <- imap(plotData, ~ {
+  ggplot(.x, aes(x = p, y = fct_reorder(Description, p))) +
+    geom_col(aes(alpha = p), fill = "black", width = .8, show.legend = F) +
+    scale_alpha_continuous(range = c(.5, 1)) +
+    scale_x_continuous(expand = expansion(c(0, 0.05))) +
+    labs(x = TeX("$-log_{10}(\\textit{P}\\,value)$"), y = "", title = .y) +
+    theme(
+      aspect.ratio = 0.75,
+      panel.background = element_blank(),
+      panel.grid = element_blank(),
+      axis.line = element_line())
+})
+
+purrr::reduce(GOplot, `+`) + plot_layout(ncol = 1)
+
+ggsave("graphics/GO.png", width = 10, height = 4 * length(GOfile))
---
+++ bulkRNAana_3_GO.R
@@ -0,0 +1,53 @@
+
+# MESSAGE -----------------------------------------------------------------
+#
+# author: Yulin Lyu
+# email: lvyulin@pku.edu.cn
+#
+# require: R whatever
+#
+# ---
+
+# * 1. Load packages ------------------------------------------------------
+
+setwd("exampleData/RNA")
+
+# grammar
+library(tidyverse)
+library(magrittr)
+library(glue)
+library(data.table)
+
+# analysis
+library(DESeq2)
+library(org.Hs.eg.db)
+library(clusterProfiler)
+
+# * 2. Load data ----------------------------------------------------------
+
+anno <- readRDS("../../data/hg19anno.rds")
+
+diffData <- fread("DESeq2/XF_vs_F.DEG.csv")
+colnames(diffData)[1] <- "gene"
+
+diffData[is.na(padj), padj := 1][]
+
+# * 3. Analyze ------------------------------------------------------------
+
+diffData[, type := "ns"][]
+diffData[log2FoldChange > 1 & padj < 0.05, type := "up"][log2FoldChange < -1 & padj < 0.05, type := "down"][]
+
+geneList <- list()
+geneList$XFup <- diffData[type == "up", gene]
+geneList$Fup <- diffData[type == "down", gene]
+
+egoList <- map(geneList, ~ {
+  enrichGO(
+    gene = na.omit(select(org.Hs.eg.db, keys = .x, columns = "ENTREZID", keytype = "SYMBOL")$ENTREZID),
+    OrgDb = "org.Hs.eg.db", ont = "BP", pvalueCutoff = 1, qvalueCutoff = 1, readable = T)
+})
+names(egoList)
+
+iwalk(egoList, ~ write.csv(.x@result, paste0(.y, ".GO.csv")))
+
+
